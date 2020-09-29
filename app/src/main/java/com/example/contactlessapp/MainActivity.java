08b2e541127970d819951d0579629ec444322e09
alberthehuman <raymundlb@gmail.com>
2020-09-29 21:01:14 +0800

    Firebase connected while Forgot Password and Create Account in progress

---
+++ app/src/main/java/com/example/contactlessapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.contactlessapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText username_input = findViewById(R.id.username_xml);
+        EditText password_input = findViewById(R.id.password_xml);
+        Button login_input = findViewById(R.id.login_xml);
+        TextView forgotpass_input = findViewById(R.id.forgotpass_xml);
+        TextView createaccount_input = findViewById(R.id.createaccount_xml);
+
+        forgotpass_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ForgotPassActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this,"you clicked forgot pass",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        createaccount_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CreateAccountActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this,"you clicked create account",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
