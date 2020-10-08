ca3e6167a95621e37b9adcb3a1de6cb58d734eba
nayr17 <ryanpanes68@gmail.com>
2020-10-08 20:35:18 +0800

    connected to firebase database. working on checking username if already exist..

--- app/src/main/java/com/example/contactlessapp/CreateAccountCustomerActivity.java
+++ app/src/main/java/com/example/contactlessapp/CreateAccountCustomerActivity.java
@@ -1,18 +1,29 @@
 package com.example.contactlessapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.contactlessapp.DbHelpers.CreateAccountCustomerHelperClass;
 
 import android.content.Intent;
+import android.icu.util.Freezable;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.contactlessapp.DbHelpers.CreateAccountShopHelperClass;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class CreateAccountCustomerActivity extends AppCompatActivity {
     EditText CustomerName;
@@ -24,9 +35,11 @@ public class CreateAccountCustomerActivity extends AppCompatActivity {
     EditText CustomerPass;
     EditText CustomerConfirmPassword;
 
+
     String AccountType ="Customer";
-    FirebaseDatabase myDB;
-    DatabaseReference users;
+    Integer id;
+    Button btncreate;
+
 
 
 
@@ -44,11 +57,15 @@ public class CreateAccountCustomerActivity extends AppCompatActivity {
         CustomerPass = findViewById(R.id.editTextCustomerPassword);
         CustomerConfirmPassword = findViewById(R.id.editTextCustomerConfirmPassword);
 
+
+
     }
 
     public void btnCustomerCreateAccount (View view) {
-       myDB = FirebaseDatabase.getInstance();
-       users = myDB.getReference("Users");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myref = database.getReference("Registered_Users");
+
 
        String accountType = AccountType;
        String name = CustomerName.getText().toString().trim();
@@ -61,12 +78,84 @@ public class CreateAccountCustomerActivity extends AppCompatActivity {
        String confirmpass = CustomerConfirmPassword.getText().toString().trim();
 
 
-           CreateAccountCustomerHelperClass helperClass = new CreateAccountCustomerHelperClass( accountType, name, address, phoneNumber, barangay, username, email, password);
-           users.child(username).setValue(helperClass);
-           Intent intent = new Intent(this, MainActivity.class);
-           startActivity(intent);
-           Toast.makeText(this, "Account successfully created!", Toast.LENGTH_SHORT).show();
-           finish();
+       if(CustomerName.length() ==0){
+           CustomerName.setError("field cannot be empty.");
+       }
+       if(CustomerAddress.length()==0){
+           CustomerAddress.setError("field cannot be empty.");
+       }
+       if(CustomerPhoneNumber.length()==0){
+           CustomerPhoneNumber.setError("field cannot be empty.");
+       }
+       if(CustomerBarangay.length()==0){
+           CustomerBarangay.setError("field cannot be empty.");
+       }
+       if(CustomerUsername.length()==0){
+           CustomerUsername.setError("field cannot be empty.");
+       }
+       if(CustomerEmail.length()==0){
+           CustomerEmail.setError("field cannot be empty.");
+       }
+       if(CustomerPass.length()==0){
+           CustomerPass.setError("field cannot be empty.");
+       }
+       if(CustomerConfirmPassword.length()==0){
+           CustomerConfirmPassword.setError("field cannot be empty.");
+       }
+       else if (password != confirmpass) {
+            Toast.makeText(this, "password does not match", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+       if(password.equals(confirmpass)){
+           if(password != null){
+               if(password != null){
+
+                   DatabaseReference checkUser = FirebaseDatabase.getInstance().getReference("Registered_Users");
+                   checkUser.orderByChild("username").equalTo(username);
+                   if(checkUser != null){
+                       Toast.makeText(CreateAccountCustomerActivity.this, "already exist!", Toast.LENGTH_LONG).show();
+                   }
+
+
+                   /*.addListenerForSingleValueEvent(new ValueEventListener() {
+                       @Override
+                       public void onDataChange(@NonNull DataSnapshot snapshot) {
+                           if(snapshot.getValue() != null){
+                               Toast.makeText(CreateAccountCustomerActivity.this, "already exist!", Toast.LENGTH_LONG).show();
+                           }
+                       }
+
+                       @Override
+                       public void onCancelled(@NonNull DatabaseError error) {
+
+                       }
+                   });
+
+                    */
+
+
+                      Toast.makeText(this, "wow!", Toast.LENGTH_LONG).show();
+
+                   Toast.makeText(this, "Error!", Toast.LENGTH_LONG).show();
+                    /*
+                   CreateAccountCustomerHelperClass helperClass = new CreateAccountCustomerHelperClass(accountType, name, address, phoneNumber, barangay, username, email, password);
+                   myref.child(username).setValue(helperClass);
+
+                   Intent intent = new Intent(this, MainActivity.class);
+                   startActivity(intent);
+                   Toast.makeText(this, "Account successfully created!", Toast.LENGTH_SHORT).show();
+
+                     */
+
+               }
+           }
+
+       }
+
+
+
 
 
     }
--- app/src/main/java/com/example/contactlessapp/CreateAccountShopActivity.java
+++ app/src/main/java/com/example/contactlessapp/CreateAccountShopActivity.java
@@ -1,4 +1,6 @@
 package com.example.contactlessapp;
+import com.example.contactlessapp.DbHelpers.CreateAccountCustomerHelperClass;
+import com.example.contactlessapp.DbHelpers.CreateAccountShopHelperClass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.internal.AccountType;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CreateAccountShopActivity extends AppCompatActivity {
     EditText shopName;
     EditText shopLocation;
@@ -17,6 +23,8 @@ public class CreateAccountShopActivity extends AppCompatActivity {
     EditText shopPassword;
     EditText shopConfirmPassword;
     Button shopCreateAccount;
+
+    String AccountType ="Shop";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +40,57 @@ public class CreateAccountShopActivity extends AppCompatActivity {
     }
 
     public void btnShopCreateAccount(View view) {
-        shopCreateAccount = findViewById(R.id.btnShopCreateAccount);
-        /*
 
-        code
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myref = database.getReference("Registered_Users");
 
-         */
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        Toast.makeText(this, "Account successfully created!", Toast.LENGTH_SHORT).show();
+        String accountType = AccountType;
+        String name = shopName.getText().toString().trim();
+        String location = shopLocation.getText().toString().trim();
+        String username = shopUsername.getText().toString().trim();
+        String email = shopEmail.getText().toString().trim();
+        String password = shopPassword.getText().toString().trim();
+        String confirmPassword = shopConfirmPassword.getText().toString().trim();
+
+        if(shopName.length() ==0){
+            shopName.setError("field cannot be empty.");
+        }
+        if(shopLocation.length()==0){
+            shopLocation.setError("field cannot be empty.");
+        }
+       if(shopUsername.length()==0){
+            shopUsername.setError("field cannot be empty.");
+        }
+       if(shopEmail.length()==0){
+            shopEmail.setError("field cannot be empty.");
+        }
+        if(shopPassword.length()==0){
+            shopPassword.setError("field cannot be empty.");
+        }
+        if(shopConfirmPassword.length()==0){
+            shopConfirmPassword.setError("field cannot be empty.");
+        }
+        else if (password != confirmPassword){
+            Toast.makeText(this, "password does not match", Toast.LENGTH_SHORT).show();
+        }
+
+        if(password.equals(confirmPassword)){
+            if(password.length()!=0){
+                if(password.length()!=0){
+
+                        CreateAccountShopHelperClass helperClass = new CreateAccountShopHelperClass(accountType, name, location, username, email, password);
+                        myref.child(username).setValue(helperClass);
+
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(this, "Account successfully created!", Toast.LENGTH_SHORT).show();
+
+                } 
+            }
+
+
+        }
+
     }
 
     public void goToLogin(View view) {
